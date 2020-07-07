51fb5754ecd75054ae2e0acdf89719e909e0706a
Snowman-s <snowman.snowsnow@gmail.com>
2020-07-07 19:08:53 +0900

    弾の大きさを修正

--- src/com/snowsnowgmail/snowman/barrage/BarrageImageCreater.java
+++ src/com/snowsnowgmail/snowman/barrage/BarrageImageCreater.java
@@ -7,8 +7,8 @@ public final class BarrageImageCreater {
     public static BufferedImage imageCreate(int size, List<Bullet> bullets) {
         BufferedImage bufferedImage = new BufferedImage(size, size, BufferedImage.TYPE_INT_RGB);
         for (Bullet bullet : bullets) {
-            for (int i = -8; i <= 8; i++) {
-                for (int i2 = -8; i2 <= 8; i2++) {
+            for (int i = -2; i <= 2; i++) {
+                for (int i2 = -2; i2 <= 2; i2++) {
                     try {
                         bufferedImage.setRGB((int) bullet.location.x + i, (int) bullet.location.y + i2, bullet.color.getRGB());
                     }catch (ArrayIndexOutOfBoundsException e){
